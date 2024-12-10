71913abc33c6594c6ec3bf7513ab0b158c1c5b35
GDMontsho <montshogoitseone@gmail.com>
2024-12-10 13:51:28 +0100

    starter folders

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import "dotenv/config";
+import { connect } from "./config/db.js";
 
+connect();
 const app = express();
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
