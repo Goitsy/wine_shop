bfc87ff25e74d5a5e05af9f2fd8aef3c1b6cf017
GDMontsho <montshogoitseone@gmail.com>
2024-12-11 10:01:32 +0100

    user auth and val

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
-
 import express from "express";
 import "dotenv/config";
 import { connect } from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 connect();
 const app = express();
+
+app.use(express.json());
+app.use("/api/users", userRoutes);
+
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
